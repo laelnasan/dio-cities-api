b78622775f5a0d157991e8eab774f8ba69e358d2
Lael Nascimento Santos <lael.nasan@gmail.com>
2021-11-09 14:35:03 -0400

    add database mapping (pais -> Country)

---
+++ src/main/java/br/com/nasan/citiesapi/countries/Country.java
@@ -0,0 +1,51 @@
+package br.com.nasan.citiesapi.countries;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Country
+ */
+@Entity
+@Table(name = "pais")
+public class Country {
+  @Id
+  private long id;
+
+  @Column(name = "nome")
+  private String name;
+
+  @Column(name = "nome_pt")
+  private String name_pt;
+
+  @Column(name = "sigla")
+  private String code;
+
+  private int bacen;
+
+  public Country() {
+  } // empty constructor
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getName_pt() {
+    return name_pt;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public int getBacen() {
+    return bacen;
+  }
+
+}
