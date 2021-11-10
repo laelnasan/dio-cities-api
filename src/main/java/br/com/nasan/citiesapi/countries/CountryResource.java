d2842a2874d9397747da372f4e3d21e381d5db76
Lael Nascimento Santos <lael.nasan@gmail.com>
2021-11-09 20:18:33 -0400

    change api to return 404 on nonexistent id
    This is quite an interesting situation. I wonder if there is a better
    way to implement this behaviour.

--- src/main/java/br/com/nasan/citiesapi/countries/CountryResource.java
+++ src/main/java/br/com/nasan/citiesapi/countries/CountryResource.java
@@ -1,9 +1,13 @@
 package br.com.nasan.citiesapi.countries;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +24,15 @@ class CountryResource {
   @GetMapping
   public Page<Country> countries(Pageable page) {
     return repository.findAll(page);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity getOne(@PathVariable Long id) {
+    Optional<Country> optional = repository.findById(id);
+
+    return optional.isPresent()
+        ? ResponseEntity.ok().body(optional.get())
+        : ResponseEntity.notFound().build();
   }
 
 }
