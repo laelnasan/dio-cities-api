46698b08f9e94e0de3fca857f31c70db8cd21c2f
Lael Nascimento Santos <lael.nasan@gmail.com>
2021-11-13 10:41:35 -0400

    Add distance-to using byPoint method

--- src/main/java/br/com/nasan/citiesapi/cities/CityResource.java
+++ src/main/java/br/com/nasan/citiesapi/cities/CityResource.java
@@ -1,5 +1,9 @@
 package br.com.nasan.citiesapi.cities;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nasan.citiesapi.cities.entity.City;
@@ -28,11 +33,38 @@ class CityResource {
 
   @GetMapping("/{id}")
   public ResponseEntity getOne(@PathVariable Long id) {
-    Optional<City> optional = repository.findById(id);
+    Optional<City> optionalCity = repository.findById(id);
 
-    return optional.isPresent()
-        ? ResponseEntity.ok().body(optional.get())
+    return optionalCity.isPresent()
+        ? ResponseEntity.ok().body(optionalCity.get())
         : ResponseEntity.notFound().build();
+  }
+
+  @GetMapping("/{from}/distance-to/{to}")
+  public ResponseEntity getDistance(@PathVariable Long from, @PathVariable Long to,
+      @RequestParam(required = false) Boolean showcities) {
+    Optional<City> optionalCityFrom = repository.findById(from);
+    Optional<City> optionalCityTo = repository.findById(to);
+
+    if (optionalCityFrom.isPresent() && optionalCityTo.isPresent()) {
+      City cityFrom = optionalCityFrom.get();
+      City cityTo = optionalCityTo.get();
+
+      Map<String, Object> response = new HashMap<String, Object>();
+      response.put("distace-by-points", repository.distanceByPoints(cityFrom.getId(), cityTo.getId()));
+
+      if (showcities != null && showcities) {
+        List<City> cities = new ArrayList<City>();
+        cities.add(cityFrom);
+        cities.add(cityTo);
+
+        response.put("cities", cities);
+      }
+
+      return ResponseEntity.ok().body(response);
+    }
+
+    return ResponseEntity.notFound().build();
   }
 
 }
--- src/main/java/br/com/nasan/citiesapi/cities/repository/CityRepository.java
+++ src/main/java/br/com/nasan/citiesapi/cities/repository/CityRepository.java
@@ -1,6 +1,7 @@
 package br.com.nasan.citiesapi.cities.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.nasan.citiesapi.cities.entity.City;
 
@@ -9,4 +10,9 @@ import br.com.nasan.citiesapi.cities.entity.City;
  */
 public interface CityRepository extends JpaRepository<City, Long> {
 
+  @Query(nativeQuery = true, value = "SELECT ((SELECT lat_lon FROM cidade WHERE id = ?1) <@> (SELECT lat_lon FROM cidade WHERE id = ?2)) as distance")
+  Double distanceByPoints(final Long cityId1, final Long cityId2);
+
+  @Query(nativeQuery = true, value = "SELECT earth_distance(ll_to_earth(?1, ?2), ll_to_earth(?3, ?4)) as distance")
+  Double distanceByCube(final Double la1, final Double lo1, final Double la2, final Double lo2);
 }
